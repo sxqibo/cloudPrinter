fe36dfdbdc8ed32972f71b33367631f066f7c88f
yun <1427596852@qq.com>
2023-04-25 12:11:31 +0800

    modify .gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sxqibo/cloudprinter',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'aa86cffe0ab689e0399965d8a2d0facec5f00aa0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sxqibo/cloudprinter' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'aa86cffe0ab689e0399965d8a2d0facec5f00aa0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
