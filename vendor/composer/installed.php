5f89606c1052592f19605ec641f4ef47447a4876
yun <1427596852@qq.com>
2023-04-25 15:23:07 +0800

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'sxqibo/cloudprinter',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'fe36dfdbdc8ed32972f71b33367631f066f7c88f',
+        'reference' => '7c9b7ba438b8a4aa78c14b133dd954a60dffa041',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'sxqibo/cloudprinter' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'fe36dfdbdc8ed32972f71b33367631f066f7c88f',
+            'reference' => '7c9b7ba438b8a4aa78c14b133dd954a60dffa041',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
