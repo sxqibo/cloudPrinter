59a223195c49ddb652da62cba424c34b43a8f670
hongwei <403236160@qq.com>
2023-05-22 18:55:37 +0800

    fix

--- sample/demo.php
+++ sample/demo.php
@@ -1,21 +1,25 @@
 <?php
-require __DIR__ . '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$config = [
-    'user'        => '', // 飞鹅云注册账号
-    'ukey'        => 'YqRM2UyHmxdbbEqq', // 飞鹅云开发者 UKEY
-    'callBackUrl' => '', // 打印机回调地址
-];
+try {
+    $config = [
+        'user'        => '', // 飞鹅云注册账号
+        'ukey'        => '', // 飞鹅云开发者 UKEY
+        'callBackUrl' => '', // 打印机回调地址
+    ];
 
-// 打印机 SN 号
-$sn = '960816217';
+    // 打印机 SN 号
+    $sn = '';
 
-// 打印内容
-$content = '<C>测试打印</C>';
-$content .= '<C>山西岐伯科技</C>';
+    // 打印内容
+    $content = '<C>测试打印</C>';
+    $content .= '<C>山西岐伯科技</C>';
 
-// 打印几联
-$times = 1;
+    // 打印几联
+    $times = 1;
 
-$feie = new FeieYun($config);
-$feie->printMsg($sn, $content, $times);
+    $feie = new \sxqibo\cloudprinter\feieyun\FeieYun($config);
+    $feie->printMsg($sn, $content, $times);
+} catch (\Exception $e) {
+    print_r($e->getMessage());
+}
